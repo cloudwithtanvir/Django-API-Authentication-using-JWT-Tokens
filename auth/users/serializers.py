e3532792ea4274ab46bf76214d4f2d4295a938ad
tanvirewu <tanvirewu32@gmail.com>
2021-10-05 05:36:59 -0400

    validating password

--- auth/users/serializers.py
+++ auth/users/serializers.py
@@ -4,3 +4,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields  = ['id', 'name', 'email','password']
+        extra_kwargs ={
+            'password':{'write_only':True}
+        }
+
+def create(self, validated_data):
+    password = validated_data.pop('password',None) 
+    isinstance = self.Meta.model(**validated_data)
+    if password is not None:
+        isinstance.set_password(password)
+        isinstance.save()
+        return isinstance       

618ea4edd9939057c73c284d2804930717ceb4fa
tanvirewu <tanvirewu32@gmail.com>
2021-10-05 05:33:25 -0400

    hashing the password

--- auth/users/views.py
+++ auth/users/views.py
@@ -3,8 +3,11 @@ from rest_framework import serializers
 from rest_framework.views import APIView
 
 from users.serializers import UserSerializer
+from rest_framework.exceptions import AuthenticationFailed
+
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from .models import Users
 
 # Create your views here.
 
@@ -14,3 +17,24 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self,request):
+        email = request.data['email']
+        password = request.data['password']
+
+
+        user = Users.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('Users not found !')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect Password")
+
+        return Response(
+            {
+                'message': 'success'
+            }
+        )
+

cfbe2dfd1f1382fe2c4f857ca61fa5ed57bed0eb
tanvirewu <tanvirewu32@gmail.com>
2021-10-05 02:42:43 -0400

    adding auth & creating users

---
+++ auth/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.views import APIView
+
+from users.serializers import UserSerializer
+from .serializers import UserSerializer
+from rest_framework.response import Response
+
+# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
